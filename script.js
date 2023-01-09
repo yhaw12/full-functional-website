d14d16fb6e3fc7a08f1372e747a9ee704c7112f7
yhaw12 <yooxing11@gmail.com>
2023-01-09 23:21:38 +0000

    website commit

---
+++ script.js
@@ -0,0 +1,27 @@
+ const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  autoplay:{
+    delay: 2000,
+    disableinteractions: false,
+  },
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+  // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+});
+
+
+
